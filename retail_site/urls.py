9827c40073f55f931481a83d6929debd960bfa5b
Roman Kravets <roman.kravets@vakoms.com>
2022-07-07 00:15:44 +0300

    add universities, departments, groups endpoints

--- retail_site/urls.py
+++ retail_site/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from retail_site.views import GroupLessonsAPIView, GroupTeachersAPIView, GroupSubjectsAPIView
+from retail_site.views import LessonsAPIView, GroupTeachersAPIView, GroupSubjectsAPIView, UniversityAPIView, \
+    DepartmentAPIView, GroupAPIView
 
 app_name = 'retail'
 
 urlpatterns = [
-    path('group/<int:group_id>/schedule/', GroupLessonsAPIView.as_view(), name='group-schedule'),
-    path('group/<int:group_id>/teachers/', GroupTeachersAPIView.as_view(), name='group-teachers'),
-    path('group/<int:group_id>/subjects/', GroupSubjectsAPIView.as_view(), name='group-subjects'),
+    path('universities/', UniversityAPIView.as_view(), name='universities'),
+    path('universities/<int:university_id>/departments/', DepartmentAPIView.as_view(), name='departments'),
+    path('departments/<int:department_id>/groups/', GroupAPIView.as_view(), name='groups'),
+
+    path('groups/<int:group_id>/lessons/', LessonsAPIView.as_view(), name='group-lessons'),
+    path('groups/<int:group_id>/teachers/', GroupTeachersAPIView.as_view(), name='group-teachers'),
+    path('groups/<int:group_id>/subjects/', GroupSubjectsAPIView.as_view(), name='group-subjects'),
 ]
--- retail_site/views.py
+++ retail_site/views.py
@@ -1,20 +1,49 @@
-from django.db import connection
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from admin_site.models import Lesson, Teacher, Subject
-from retail_site.serializers import LessonSerializer, TeacherShortSerializer, SubjectSerializer
+from admin_site.models import Lesson, Teacher, Subject, University, Department, Group
+from retail_site.serializers import LessonSerializer, TeacherShortSerializer, SubjectSerializer, UniversitySerializer, \
+    DepartmentSerializer, GroupSerializer
 
 
-class GroupLessonsAPIView(generics.ListAPIView):
+class UniversityAPIView(generics.ListAPIView):
+    authentication_classes = []
+    permission_classes = [AllowAny]
+    queryset = University.objects.all()
+    serializer_class = UniversitySerializer
+
+
+class DepartmentAPIView(generics.ListAPIView):
+    authentication_classes = []
+    permission_classes = [AllowAny]
+    serializer_class = DepartmentSerializer
+
+    def get_queryset(self):
+        university = get_object_or_404(University, id=self.kwargs.get('university_id'))
+        return Department.objects.filter(university=university)
+
+
+class GroupAPIView(generics.ListAPIView):
+    authentication_classes = []
+    permission_classes = [AllowAny]
+    serializer_class = GroupSerializer
+
+    def get_queryset(self):
+        department = get_object_or_404(Department, id=self.kwargs.get('department_id'))
+        return Group.objects.filter(department=department)
+
+
+class LessonsAPIView(generics.ListAPIView):
     authentication_classes = []
     permission_classes = [AllowAny]
     serializer_class = LessonSerializer
 
     def get_queryset(self):
+        group = get_object_or_404(Group, id=self.kwargs.get('group_id'))
         return (
-            Lesson.objects.filter(groups=self.kwargs.get('group_id'))
+            Lesson.objects.filter(groups=group)
             .select_related('auditorium__academy_building')
             .prefetch_related('teachers')
             .order_by('week_day', 'lesson_number')
@@ -31,7 +60,8 @@ class GroupTeachersAPIView(generics.ListAPIView):
     serializer_class = TeacherShortSerializer
 
     def get_queryset(self):
-        return Teacher.objects.filter(lessons__groups=self.kwargs.get('group_id')).distinct()
+        group = get_object_or_404(Group, id=self.kwargs.get('group_id'))
+        return Teacher.objects.filter(lessons__groups=group).distinct()
 
 
 class GroupSubjectsAPIView(generics.ListAPIView):
@@ -40,4 +70,5 @@ class GroupSubjectsAPIView(generics.ListAPIView):
     serializer_class = SubjectSerializer
 
     def get_queryset(self):
-        return Subject.objects.filter(lessons__groups=self.kwargs.get('group_id')).distinct()
+        group = get_object_or_404(Group, id=self.kwargs.get('group_id'))
+        return Subject.objects.filter(lessons__groups=group).distinct()
