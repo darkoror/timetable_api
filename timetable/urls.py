e04ae824df6bcd9329f92bb67717f498adb79a49
Roman Kravets <roman.kravets@vakoms.com>
2022-07-07 11:23:52 +0300

    refactor docs urls

--- timetable/urls.py
+++ timetable/urls.py
@@ -20,11 +20,11 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
-# docs_urlpatterns = [
-#     path('', SpectacularAPIView.as_view(), name='schema'),
-#     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
-#     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-# ]
+docs_urlpatterns = [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
 
 api_urlpatterns = [
     path('admin-site/', include('admin_site.urls')),
@@ -34,9 +34,6 @@ api_urlpatterns = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urlpatterns)),
-    # path('docs/', include(docs_urlpatterns)),
-    path('docs/schema/', SpectacularAPIView.as_view(api_version='YOUR_VERSION'), name='schema'),
-    path('docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-
+    path('docs/', include(docs_urlpatterns)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
