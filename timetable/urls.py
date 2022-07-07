ac7e23f077daf3aa72e834f8a2cd086c9b4924ca
Roman Kravets <roman.kravets@vakoms.com>
2022-07-07 11:20:03 +0300

    add drf spectacular autodocs

--- timetable/urls.py
+++ timetable/urls.py
@@ -17,15 +17,26 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
+
+# docs_urlpatterns = [
+#     path('', SpectacularAPIView.as_view(), name='schema'),
+#     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+#     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+# ]
 
 api_urlpatterns = [
     path('admin-site/', include('admin_site.urls')),
-    path('', include('retail_site.urls'))
+    path('', include('retail_site.urls')),
 ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_urlpatterns)),
+    # path('docs/', include(docs_urlpatterns)),
+    path('docs/schema/', SpectacularAPIView.as_view(api_version='YOUR_VERSION'), name='schema'),
+    path('docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
